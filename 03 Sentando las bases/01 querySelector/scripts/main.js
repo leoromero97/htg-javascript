ac10525a0f847fb3c6243f239823554f927de5e8
vivianachoque <vivianaaque@gmail.com>
2020-01-16 15:25:14 -0300

    realice ejercicios 1 y 2

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,13 @@
-console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+const comentariofav = document.querySelector('.comment');
+console.log(comentariofav);
+comentariofav.classList.add('comentariofav');
+//variable comentariofav se guarda el selector .commet y en la tercera linea se añade otro selector nuevo con nuevos estilos.
+
+const elementoHr = document.querySelectorAll('hr');
+console.log(elementoHr);
+for (let i =0;i<elementoHr.length;i++){
+    
+    elementoHr[i].classList.add('clasElemHr');
+}
+//la variable elmentoHr es un array entonces para recorrerla hay que usar un while o unn for y ahi si añadirle un estilo
+
